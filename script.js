e7ccf7ea43aa697821c075d79d835fb434515e99
Erick Saavedra <esvdrah@gmail.com>
2021-05-19 17:57:24 -0500

    Researching getting height of elements

--- script.js
+++ script.js
@@ -4,29 +4,38 @@ let accordionButton = document
         item.addEventListener('click', (event) => {
             console.log('Click');
 
+            let accCollapse = item.nextElementSibling;
+
             // Open accordion item
             if (!item.classList.contains('open')) {
                 console.log('toggle accordion button');
-                item.nextElementSibling.classList =
-                    'accordion__collapse collapsing';
+
+                accCollapse.style.display = 'block';
+                let accHeight = accCollapse.clientHeight;
+                console.log(accHeight);
+
+                setTimeout(() => {
+                    accCollapse.style.height = accHeight + 'px';
+                    accCollapse.style.display = '';
+                }, 1);
+
+                accCollapse.classList = 'accordion__collapse collapsing';
 
                 setTimeout(() => {
                     console.log('open accordion content');
-                    item.nextElementSibling.classList =
-                        'accordion__collapse open';
+                    accCollapse.classList = 'accordion__collapse collapse open';
+                    accCollapse.style.height = '';
                 }, 300);
             }
             // Close accordion item
             else {
                 // Remove "collapse open" from .accordion__collapse, add "collapsing"
-                item.nextElementSibling.calssList =
-                    'accordion__collapse collapsing';
+                accCollapse.calssList = 'accordion__collapse collapsing';
 
                 // After X amount of time, remove "collapsing" class and add "collapse" class
                 setTimeout(() => {
                     console.log('close accordion content');
-                    item.nextElementSibling.classList =
-                        'accordion__collapse collapse';
+                    accCollapse.classList = 'accordion__collapse collapse';
                 }, 300);
             }
 
